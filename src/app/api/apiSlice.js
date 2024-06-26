e250e1caec00c80b2d1c1e87211101f3609a69e1
biodota1 <111911659+biodota1@users.noreply.github.com>
2024-06-26 06:14:04 -0700

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:2777",
+  baseUrl: "https://mrtech-be.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
